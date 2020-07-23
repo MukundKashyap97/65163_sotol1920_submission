a53c7ac37b9e60051015ec4a1eeb799ce6498af0
Venkata Mukund Kashyap Yedunuthala <mukundkashyapyn@outlook.com>
2020-07-23 14:33:07 +0200

    updates objects.py, adds Sphere class

--- src/pygeo/objects.py
+++ src/pygeo/objects.py
@@ -2,7 +2,10 @@ import numpy as np
 
 
 class Point:
-    """A point."""
+    """ Class Point
+        Defines a point in 3-dimensional Euclidean space.
+        Requires a list/tuple/numpy array as input
+        Methods implemented: Addition, Subtraction, Equality"""
 
     def __init__(self, point):
         self._point = np.array(point, dtype=float)
@@ -32,13 +35,17 @@ class Point:
 
 
 class Vector:
-    """A vector."""
+    """ Class Vector.
+        Defines a vector in 3-dimensional Euclidean space.
+        Requires list/tuple/numpy array as input,
+        which defines the components of vector along each coordinate axis.
+        Methods implemented: Addition, Subtraction, Equality"""
 
     def __init__(self, vector):
         self._vector = np.array(vector, dtype=float)
 
     def __repr__(self):
-        return f"Point({self._vector.tolist()})"
+        return f"Vector({self._vector.tolist()})"
 
     def __add__(self, other):
         if isinstance(other, Vector):
@@ -58,14 +65,24 @@ class Vector:
 
 class Ray:
     """A ray."""
-
-    ...
+    def __init__(self):
+        ...
+    def __repr__(self):
+        ...
+    def __eq__(self, value):
+        ...
 
 
 class Sphere:
-    """A sphere."""
+    """ Class Sphere
+        Requires list/tuple/numpy array (center) and a float/double/string as input (radius)
+        Methods implemented: Equality """
 
-    ...
+    def __init__(self, center, radius):
+        self._center = np.array(center,dtype=float)
+        self._radius = float(radius)
+    def __repr__(self):
+        return f"Sphere(Center:{self._center.tolist()}, Radius:{self._radius})"
 
 
 class Triangle:
