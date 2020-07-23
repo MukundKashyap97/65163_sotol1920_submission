64326585187a810ee4518297447f0de37f1f50d9
Venkata Mukund Kashyap Yedunuthala <mukundkashyapyn@outlook.com>
2020-07-23 14:59:45 +0200

    updates intersect, tests

--- src/pygeo/intersect.py
+++ src/pygeo/intersect.py
@@ -1,4 +1,5 @@
-from .objects import Ray, Sphere, Triangle
+from .objects import Point, Ray, Sphere, Triangle
+from numpy import subtract,dot,sqrt,array,all
 
 
 def intersect(first_object, second_object):
@@ -6,8 +7,19 @@ def intersect(first_object, second_object):
 
 
 def _intersect_ray_with_sphere(ray, sphere):
-    ...
-
-
+    difference_o_c = subtract(ray._origin,sphere._center)
+    nabla = (dot(ray._direction,difference_o_c))**2 - ((dot(difference_o_c,difference_o_c)) - sphere._radius**2)
+    if nabla < 0:
+        print("Specified ray and sphere do not intersect")
+        return None
+    elif nabla == 0:
+        d = -1*dot(ray._direction,difference_o_c)
+        return array([(ray._origin) + (d*ray._direction)])
+    else:
+        d = array([-1*dot(ray._direction,difference_o_c)-sqrt(nabla),-1*dot(ray._direction,difference_o_c)+sqrt(nabla)])
+        points = []
+        for d in d[d>=0]:
+            points.append((ray._origin) + (d*ray._direction))
+        return array(points)
 def _intersect_ray_with_triangle(ray, triangle):
     ...
--- tests/test_intersect.py
+++ tests/test_intersect.py
@@ -4,12 +4,34 @@ from pygeo.intersect import (
     _intersect_ray_with_triangle,
 )
 from pygeo.objects import Ray, Sphere, Triangle
-
+from numpy import array_equal
 
 # intersect
 
 
 # _intersect_ray_with_sphere
+def test__intersect_ray_with_sphere__sphere_center_at_origin__given_tangent_ray__return_true():
+    assert array_equal(_intersect_ray_with_sphere(Ray((-3,1,0),(1,0,0)),Sphere((0,0,0),1.0)),[[0,1,0]]) is True
 
+def test__intersect_ray_with_sphere__sphere_center_at_origin__given_ray_that_intersects_at_two_points__return_true():
+    assert array_equal(_intersect_ray_with_sphere(Ray((-2,0,0),(1,0,0)),Sphere((0,0,0),1.0)),[[-1,0,0],[1,0,0]]) is True
+
+def test__intersect_ray_with_sphere__sphere_center_at_origin__given_ray_that_intersects_at_no_points__return_true():
+    assert (_intersect_ray_with_sphere(Ray((-2,1.5,0),(1,0,0)),Sphere((0,0,0),1.0)) == None)
+
+def test__intersect_ray_with_sphere__sphere_center_at_origin__given_ray_that_originates_inside_sphere__return_true():
+    assert array_equal(_intersect_ray_with_sphere(Ray((-0.5,0,0),(1,0,0)),Sphere((0,0,0),1.0)),[[1,0,0]]) is True
+
+def test__intersect_ray_with_sphere__sphere_center_elsewhere__given_tangent_ray__return_true():
+    assert array_equal(_intersect_ray_with_sphere(Ray((-3,1,0),(1,0,0)),Sphere((1,1,1),1.0)),[[1,1,0]]) is True
+
+def test__intersect_ray_with_sphere__sphere_center_elsewhere__given_ray_that_intersects_at_two_points__return_true():
+    assert array_equal(_intersect_ray_with_sphere(Ray((-2,1,1),(1,0,0)),Sphere((1,1,1),1.0)),[[0,1,1],[2,1,1]]) is True
+
+def test__intersect_ray_with_sphere__sphere_center_elsewhere__given_ray_that_intersects_at_no_points__return_true():
+    assert (_intersect_ray_with_sphere(Ray((-2,1.5,0),(1,0,0)),Sphere((1,1,1),1.0)) == None)
+
+def test__intersect_ray_with_sphere__sphere_center_elsewhere__given_ray_that_originates_inside_sphere__return_true():
+    assert array_equal(_intersect_ray_with_sphere(Ray((1,1,1),(0,0,1)),Sphere((1,1,1),1.0)),[[1,1,2]]) is True
 
 # _intersect_ray_with_triangle
