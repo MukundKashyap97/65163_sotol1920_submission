715e847b9f6d6fa3049815d3c9c9922e0cf42d5e
Venkata Mukund Kashyap Yedunuthala <mukundkashyapyn@outlook.com>
2020-07-23 15:00:57 +0200

    updates intersect

--- src/pygeo/intersect.py
+++ src/pygeo/intersect.py
@@ -7,6 +7,11 @@ def intersect(first_object, second_object):
 
 
 def _intersect_ray_with_sphere(ray, sphere):
+    """ Method to determine intersection of a Ray object and a sphere object.
+        Input: ray (of class Ray), sphere (of class Sphere)
+        returns:
+            0: if there is no intersection
+            NumPy array (of objects of Point class): if an intersection exists """
     difference_o_c = subtract(ray._origin,sphere._center)
     nabla = (dot(ray._direction,difference_o_c))**2 - ((dot(difference_o_c,difference_o_c)) - sphere._radius**2)
     if nabla < 0:
@@ -14,12 +19,12 @@ def _intersect_ray_with_sphere(ray, sphere):
         return None
     elif nabla == 0:
         d = -1*dot(ray._direction,difference_o_c)
-        return array([(ray._origin) + (d*ray._direction)])
+        return Point((ray._origin) + (d*ray._direction))
     else:
         d = array([-1*dot(ray._direction,difference_o_c)-sqrt(nabla),-1*dot(ray._direction,difference_o_c)+sqrt(nabla)])
         points = []
         for d in d[d>=0]:
-            points.append((ray._origin) + (d*ray._direction))
+            points.append(Point((ray._origin) + (d*ray._direction)))
         return array(points)
 def _intersect_ray_with_triangle(ray, triangle):
     ...
